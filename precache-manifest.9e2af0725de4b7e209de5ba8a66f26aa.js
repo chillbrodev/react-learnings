b33fb92a93648eb9d13be9546fa87e5b8715f8b7
Jon Holtan <jholtan@tribalscale.com>
2019-01-24 12:22:55 -0800

    Updates

---
+++ precache-manifest.9e2af0725de4b7e209de5ba8a66f26aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "4cad1a72c628c6075217",
+    "url": "/react-tutorial/static/js/main.4cad1a72.chunk.js"
+  },
+  {
+    "revision": "f4789f78bc5099d1ff6c",
+    "url": "/react-tutorial/static/js/1.f4789f78.chunk.js"
+  },
+  {
+    "revision": "4cad1a72c628c6075217",
+    "url": "/react-tutorial/static/css/main.534b985d.chunk.css"
+  },
+  {
+    "revision": "fb5f4bdfb877580131b2ab2a206c45c7",
+    "url": "/react-tutorial/index.html"
+  }
+];
